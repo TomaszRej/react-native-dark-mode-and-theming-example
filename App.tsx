b9859c63b4c6779ec60f3f3c54b50c2be4d0bd27
tomaszrej <tomasz.rej@netguru.com>
2021-08-26 10:00:06 +0200

    refactor

--- App.tsx
+++ App.tsx
@@ -1,40 +1,23 @@
 import * as React from 'react';
-import {useColorScheme} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {darkTheme, lightTheme} from './src/themes/theme';
-import HomeScreen from './src/screens/HomeScreen';
-import DetailsScreen from './src/screens/DetailsScreen';
-import RootNavigation from './src/navigation/RootNavigation';
-
-export enum SCREENS {
-  Home = 'Home',
-  Details = 'Details',
-}
-
-export type RootStackParamList = {
-  [SCREENS.Home]: undefined;
-  [SCREENS.Details]: undefined;
-};
-
-const Stack = createStackNavigator<RootStackParamList>();
+import {SafeAreaView, Text, View, StyleSheet} from 'react-native';
 
 const App = () => {
-  const scheme = useColorScheme();
-
   return (
-    <RootNavigation />
-    // <NavigationContainer theme={scheme === 'dark' ? darkTheme : lightTheme}>
-    //   <Stack.Navigator initialRouteName={SCREENS.Home}>
-    //     <Stack.Screen
-    //       name={SCREENS.Home}
-    //       component={HomeScreen}
-    //       options={{title: 'Overview'}}
-    //     />
-    //     <Stack.Screen name={SCREENS.Details} component={DetailsScreen} />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
+    <SafeAreaView>
+      <View style={styles.wrapper}>
+        <Text style={styles.text}>Hello</Text>
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  wrapper: {
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+});
 
 export default App;
